76743210d39ddec4128b405061b5401597d72f72
Mikki797 <40756204+Mikki797@users.noreply.github.com>
2021-06-20 23:43:29 +0300

    Rename some columns in tables, add fillfactor in choices table

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,12 +1,14 @@
 CREATE TABLE polls (
-        id serial PRIMARY KEY,
-        name text NOT NULL
-    );
+        choice_id serial PRIMARY KEY,
+        name text NOT NULL CHECK (name <> '')
+);
 
 CREATE TABLE choices (
-        id integer NOT NULL,
+        choice_id integer NOT NULL,
         poll_id integer REFERENCES polls,
-        name text NOT NULL,
-        votes_number integer NOT NULL,
-        PRIMARY KEY (id, poll_id)
-    );
+        name text NOT NULL CHECK (name <> ''),
+        vote_count integer NOT NULL,
+        PRIMARY KEY (id, poll_id),
+        UNIQUE (poll_id, name)
+)
+WITH (fillfactor=70);

b0705c68105c552607b518c0faba2958314f8e12
Mikki797 <40756204+Mikki797@users.noreply.github.com>
2021-06-21 17:41:36 +0300

    Add vote and get_result functions

--- app/api.py
+++ app/api.py
@@ -6,24 +6,41 @@ import json
 
 from datetime import datetime
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from typing import Dict, Any, Optional
 
-from app.api_types import PollType, VoteType
+from app.api_types import PollType, VoteType, PollOutType, ResultType, ChoiceResultType
 from app.database import Database
 
 
 app = FastAPI(title='avito-test')
 db = Database()
 
+
 @app.post('/api/createPoll',
-          response_description="Id добавленного голосования")
-async def create_poll(poll: PollType) -> Dict[str, Any]:
+          response_description="Id добавленного голосования",
+          response_model=PollOutType)
+async def create_poll(poll: PollType):
     poll_id = db.add_poll(poll)
-    return {'status': 'ok', 'id': poll_id}
+    return PollOutType(id=poll_id)
 
 
+# TODO добавить голосование за несколько вариантов
 @app.post('/api/poll',
-          response_description="Результат голосования за конкретный вариант"
-                               "(False при отсутствии такого голосования или варианта, иначе True")
-async def poll(id: int, date1: Optional[int] = None, date2: Optional[int] = None) -> Dict[str, Any]:
-    return {'status': 'ok', 'result': ''}
+          response_description="Проголосовать за вариант в голосовании")
+async def vote(vote: VoteType):
+    if not db.vote(vote):
+        raise HTTPException(status_code=404, detail='Poll or choice not found')
+    return {'status': 'ok'}
+
+
+@app.post('/api/getResult/{poll_id}',
+          response_description="Получить результаты голосования",
+          response_model=ResultType)
+async def get_result(poll_id: int):
+    results = db.get_result(poll_id)
+    if not results:
+        raise HTTPException(status_code=404, detail='Poll not found')
+    return ResultType(poll_id=poll_id,
+                      result=[ChoiceResultType(choice_id=choice_id, vote_count=vote_count)
+                              for choice_id, vote_count in results.items()])
